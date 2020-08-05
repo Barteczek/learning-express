a7b5e4420f399aa63a6ce129c6402c57921635ce
Bartosz Łepski <lepski.bartosz@gmail.com>
2020-08-05 21:58:07 +0200

    Implement handlebars and add dark mode

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
-app.use('/user', (req, res, next) => {
-  res.show('forbidden.html');
+app.use('/user', (req, res) => {
+  res.render('forbidden');
 });
 
 app.use(express.static('public'));
 
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
+
 app.get('/', (req, res) => {
-  res.show('home.html');
+  res.render('home');
 });
 
 app.get('/home', (req, res) => {
-  res.show('home.html');
+  res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark' });
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
+  res.render('404');
 })
 
 app.listen(3000, () => {
